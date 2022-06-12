a7da024f3dcfd545a5e9463c31a451616c808f07
BrotherIndeed <the.underdog420@gmail.com>
2022-06-13 00:09:39 +0530

    glad file bug fix

--- src/glad.c
+++ src/glad.c
@@ -22,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include </home/obliterator/Euclid_Engine/src/glad/glad.h>
+#include <glad/glad.h>
 
 static void* get_proc(const char *namez);
 
